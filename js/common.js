63a679df51e887ef10a3532004ca67dcc33a0bf2
Mikita Khadarovich <nicitosovich@mail.ru>
2017-10-02 17:29:44 +0300

    Minor changes

--- js/common.js
+++ js/common.js
@@ -1,11 +1,21 @@
-$(".select-container__a").click(function (e) {
+$(".select-container__a").on("click", function (e) {
     e.preventDefault();
-    $(".select-container__ul").css("display", 'block');
+    var dropDown = this.nextElementSibling;
+    if(dropDown.tagName == "UL") {
+      if(dropDown.classList.contains("show")) {
+        dropDown.classList.remove("show");
+      } else {
+        dropDown.classList.toggle("show");
+      }
+    }
+    e.stopPropagation();
 });
 
 $(window).click(function (e) {
     e.preventDefault();
-    if (!event.target.matches(".select-container__a")){
-        $(".select-container__ul").css("display", 'none');
+    if (event.target.matches(".select-container__a") || event.target.matches(".select-container__ul")){
+      return;
+    } else {
+      $(".select-container__ul").removeClass("show");
     }
 });
