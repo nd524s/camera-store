55d0c689a5ada1b1f5d8181a06372b5697b6c324
Mikita Khadarovich <nicitosovich@mail.ru>
2017-10-05 16:42:49 +0300

    Mobile menus

--- js/common.js
+++ js/common.js
@@ -2,26 +2,45 @@ $(".select-container__a").on("click", function (e) {
     e.preventDefault();
     var dropDown = this.nextElementSibling;
     if(dropDown.tagName == "UL") {
-      if(dropDown.classList.contains("show")) {
-        dropDown.classList.remove("show");
-      } else {
-        dropDown.classList.toggle("show");
-      }
+        if(dropDown.classList.contains("show")) {
+            dropDown.classList.remove("show");
+        } else {
+            dropDown.classList.toggle("show");
+        }
     }
     e.stopPropagation();
 });
 
 $(window).click(function (e) {
     e.preventDefault();
-    if (event.target.matches(".select-container__a") || event.target.matches(".select-container__ul")){
-      return;
+    if (event.target.matches(".select-container__a") || event.target.matches(".select-container__ul") || event.target.matches("#menu-bar")){
+        return;
     } else {
-      $(".select-container__ul").removeClass("show");
+        $(".select-container__ul").removeClass("show");
+        if(window.innerWidth <= 500) {
+            $("#item-types").css("display","none");
+      }
     }
 });
 
 $(".content__item").hover(function() {
     if($(this).closest(".content__item_large").length === 0) {
         $(this).css("background-color", "rgba(0, 99, 255, 0.3)");
+        $(this).css("border", "1px solid #251cff");
+        $(this).css("cursor", "pointer");
     }
+}, function() {
+    $(this).css("background-color", "");
+    $(this).css("border", "");
 });
+
+$("#menu-bar").on("click", function() {
+    // $("#item-types").addClass("show");
+     $("#item-types").css("display","block");
+});
+
+// $("#search-criteria").on("click", function(e) {
+//     e.preventDefault();
+//     $(".popup").css("display", "block");
+//     $(".search-box").css("display", "block");
+// });
